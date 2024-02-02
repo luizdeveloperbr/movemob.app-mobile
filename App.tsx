dd5a0876bcb3e12fcf999fc8b7844e8c693b0bb9
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2024-02-01 23:37:24 -0300

    split components

--- App.tsx
+++ App.tsx
@@ -1,34 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import InputPatrimonioId from './components/InputPatrimonioIdComponent';
 
 export default function App() {
 
-  const [patrimonioID, setPatrimonioID] = useState("")
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>CONSULTA DE PATRIMONIO</Text>
       </View>
-      <View>
-        <TextInput
-          style={styles.inputSearchID}
-          onChangeText={setPatrimonioID}
-          // onSubmitEditing={(e,s) => setPatrimonioID(s)}
-          inputMode='numeric'
-        />
-      </View>
-      <View style={styles.buttonsSearch}>
-        <Pressable style={styles.buttonSearchClean} onPress={() => { }}>
-          <Text>Limpar</Text>
-        </Pressable>
-        <Pressable style={styles.buttonSearchGo} onPress={() => { console.log(patrimonioID) }}>
-          <Text>Buscar</Text>
-        </Pressable>
-      </View>
-      <View>
-        <Text> Find: {patrimonioID}</Text>
-      </View>
+      <InputPatrimonioId />
       <StatusBar style="auto" />
     </View>
   );
@@ -45,39 +26,5 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     fontSize: 25,
-  },
-  inputSearchID: {
-    borderColor: '#000',
-    borderWidth: 2,
-    borderStyle: 'solid',
-    marginRight: 5,
-    marginLeft: 5,
-    marginBottom: 10,
-    minWidth: 300,
-    paddingLeft: 8
-  },
-  buttonsSearch: {
-    flex: 1,
-    gap: 10,
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  buttonSearchClean: {
-    height: 38,
-    paddingTop: 8,
-    paddingBottom: 8,
-    paddingRight: 16,
-    paddingLeft: 16,
-    backgroundColor: 'red',
-    borderRadius: 8
-  },
-  buttonSearchGo: {
-    height: 38,
-    paddingTop: 8,
-    paddingBottom: 8,
-    paddingRight: 16,
-    paddingLeft: 16,
-    backgroundColor: 'lime',
-    borderRadius: 8
   }
 });
---
+++ components/InputPatrimonioIdComponent.tsx
@@ -0,0 +1,83 @@
+import Reac, { Component } from 'react'
+import { StyleSheet ,View, TextInput, Text, Pressable } from 'react-native'
+
+
+export default class InputPatrimonioId extends Component {
+  inputRef: any;
+
+  state = {
+    id: 0
+  };
+
+  handleInputId(e: any) {
+    this.setState({ id: e.nativeEvent.text })
+  };
+
+  doClear() {
+    this.setState({ id: 0 })
+    this.inputRef.clear()
+  };
+
+  render(): Reac.ReactNode {
+    return <View>
+      <TextInput
+        ref={comp => this.inputRef = comp}
+        style={styles.input_id}
+        onSubmitEditing={(e) => this.handleInputId(e)}
+        inputMode='numeric'
+      />
+      <Text>{this.state.id}</Text>
+
+      <View style={{
+        flex: 1,
+        gap: 10,
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }}>
+
+        <Pressable
+          style={styles.button_clear}
+          onPress={() => this.doClear()}>
+          <Text>Limpar</Text>
+        </Pressable>
+
+        <Pressable
+          style={styles.button_focus}
+          onPress={() => this.inputRef.focus()}>
+          <Text>Focus</Text>
+        </Pressable>
+      </View>
+    </View>
+  }
+}
+
+const styles = StyleSheet.create({
+  input_id: {
+    borderColor: '#000',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    marginRight: 5,
+    marginLeft: 5,
+    marginBottom: 10,
+    minWidth: 300,
+    paddingLeft: 8
+  },
+  button_clear: {
+    height: 38,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingRight: 16,
+    paddingLeft: 16,
+    backgroundColor: 'red',
+    borderRadius: 8
+  },
+  button_focus: {
+    height: 38,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingRight: 16,
+    paddingLeft: 16,
+    backgroundColor: 'lime',
+    borderRadius: 8
+  }
+})
